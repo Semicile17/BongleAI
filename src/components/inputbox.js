33468d2010a9da4c92ac6097e143241d233b143d
Semicile17 <Rohitsinghmahant707@gmail.com>
2024-05-23 22:56:46 +0530

    added all files

--- src/components/inputbox.js
+++ src/components/inputbox.js
@@ -9,8 +9,8 @@ export default function Input() {
         className="flex-grow p-3 bg-gray-900 text-white border border-gray-700 rounded-l-lg focus:outline-none focus:border-blue-500" 
         placeholder="Type your message here..." 
       />
-      <button className="bg-gray-800 text-white p-3 rounded-r-lg">
-      <FaPaperPlane />
+      <button className="bg-gray-700 text-white p-3 rounded-r-lg border">
+      <FaPaperPlane className='h-6 w-5' />
       </button>
     </div>
     );
--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Bars3BottomRightIcon, XMarkIcon } from '@heroicons/react/24/solid';
+import { FiMenu, FiX } from 'react-icons/fi';
 
 export default function Sidebar() {
   const [isSideOpen, setIsSideOpen] = useState(false);
@@ -14,13 +14,13 @@ export default function Sidebar() {
       <div
         className={`fixed inset-y-0 left-0 transform ${
           isSideOpen ? 'translate-x-0' : '-translate-x-full'
-        } lg:translate-x-0 lg:static z-2 transition-transform duration-300 ease-in-out w-64 bg-[#3D4143] text-white`}
+        } lg:translate-x-0 lg:static z-10 transition-transform duration-300 ease-in-out w-64 bg-[#3D4143] text-white`}
       >
         <div className="p-4">
           <div className="flex justify-between items-center lg:hidden">
             <span className="text-xl font-semibold"></span>
             <button onClick={toggleSidebar}>
-              <XMarkIcon className="h-6 w-6 text-white" />
+              {isSideOpen ? <FiX className="h-6 w-6 text-white" /> : <FiMenu className="h-6 w-6 text-white" />}
             </button>
           </div>
           <div className="mt-8"></div>
@@ -34,7 +34,7 @@ export default function Sidebar() {
             className="fixed top-4 left-4 z-50 p-2 text-white"
             onClick={toggleSidebar}
           >
-            <Bars3BottomRightIcon className="h-6 w-6" />
+            <FiMenu className="h-6 w-6" />
           </button>
         )}
       </div>
